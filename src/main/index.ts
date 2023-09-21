a2a6824ec963c2ef988454c15a3a0109075cb798
anartzdev <105705996+anartzdev@users.noreply.github.com>
2023-09-21 14:29:46 +0200

    feat: fullscreen control add

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,9 +1,12 @@
 import { Map } from 'leaflet';
 import { startMapTemplate } from '../assets/template/content';
 import { tileLayerSelect } from '../config/functions';
+import { fullScreenMap } from './fullscreen';
 
 startMapTemplate(document, 'Plantilla - Mapa con Typescript');
 
 const mymap = new Map('map').setView([43.3082977, -1.9837398], 10);
 
 tileLayerSelect().addTo(mymap);
+
+fullScreenMap().addTo(mymap);
